f547c246e0f3dfc1a95e8de26e129ff9a939a572
Max Pijittum <mpijittum@fb.com>
2020-10-08 15:00:25 +0700

    Create main bot file, Add .env, Add discord library

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+print(TOKEN)
+bot = commands.Bot(command_prefix='?>')
+
+@bot.event
+async def on_ready() :
+    print("Bot Started!")
+
+@bot.event
+async def on_message(message) :
+    if message.content.startswith('?>ping') :
+       await message.channel.send('Pong ~ Meow ><')
+
+bot.run(TOKEN)
